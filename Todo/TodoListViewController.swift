b18acb42b53cb8564a19c6a0850bb1ac4ae0c481
Daisymond <daisymondchiboora@yahoo.com>
2018-08-15 23:05:58 +0800

    add new item functionality complete

--- Todo/TodoListViewController.swift
+++ Todo/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = [ "find mike", "buy eggos", "kill the demon" ]
+    var itemArray = [ "find mike", "buy eggos", "kill the demon" ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,16 +37,44 @@ class TodoListViewController: UITableViewController {
     
     // returns the selected row
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       // print(itemArray[indexPath.row])
+        // print(itemArray[indexPath.row])
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-               tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
         }
             
         else {
-               tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-       tableView.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
+    // MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todo Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // What will happen once the user clicks the add button on our UIAlert
+           
+           self.itemArray.append(textField.text!)
+        self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            // in here alertTextField is a local variable only accessible in this function, so for us to beable to trigger an action that grabs the text in the alertTextfield, in the action func,
+            // we create another textfield called textField at the begining og the addButtonPressed func which will be global, the we assign our original alertTextField to it. now we can perfom
+            // an action. good piece of skill.
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+            print(alertTextField.text)
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
